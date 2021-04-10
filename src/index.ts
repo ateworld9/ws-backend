94ad817e84807924b98b6653c8bb506bd42b8764
Dmitriy Vahrameev <vahrameev.work@gmail.com>
2021-04-11 03:12:55 +0600

    add passport oauth 2

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import cors from 'cors';
 import session from 'express-session';
 import passport from 'passport';
 
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app: Application = express();
 
 // app.use('/', (req: Request, res: Response) => {
@@ -14,13 +16,50 @@ const app: Application = express();
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(
   session({
-    secret: 'secretcode',
+    secret: process.env.SESSION_SECRET ?? 'secretcode',
     resave: true,
     saveUninitialized: true,
   }),
 );
 app.use(passport.initialize());
 app.use(passport.session());
+
+passport.serializeUser((user: any, done) => {
+  return done(null, user);
+});
+
+passport.deserializeUser((user: any, done) => {
+  return done(null, user);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+    },
+    function (accessToken: any, refreshToken: any, profile: any, cb: any) {
+      console.log('PROFILE', profile);
+
+      cb(null, profile);
+    },
+  ),
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }),
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  },
+);
 
 const PORT = process.env.PORT ?? 3001;
 app.listen(PORT, () => console.log(`The server is listening on PORT: ${PORT}`));
